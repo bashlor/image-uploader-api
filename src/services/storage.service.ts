e49c66bb8aff92a6e64a115741f98ebb38274413
Elie Samuel <bashlor@pm.me>
2022-12-03 17:53:48 +0100

    refactor: 💡 remove unused code

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -23,48 +23,13 @@ export class StorageService {
     this.BUCKET = this.configService.get<TStorageConfig>('storage').S3bucket;
   }
 
-  async getBlob(key: string): Promise<PromiseResult<S3.GetObjectOutput, AWSError>> {
-    const params = { Bucket: this.BUCKET, Key: key };
-    const blob = await this.s3.getObject(params).promise();
-
-    return blob;
-  }
-
   async putBlob(
     blobName: string,
     blob: Buffer,
   ): Promise<PromiseResult<S3.PutObjectOutput, AWSError>> {
-    const params = { Bucket: this.BUCKET, Key: blobName, Body: blob, ACL:'public-read' };
+    const params = { Bucket: this.BUCKET, Key: blobName, Body: blob, ACL: 'public-read' };
     const uploadedBlob = await this.s3.putObject(params).promise();
 
     return uploadedBlob;
-  }
-
-  async putStream(key: string, stream: Stream) {
-    const file = await new Promise<S3.PutObjectOutput>((resolve, reject) => {
-      const handleError = (error) => {
-        reject(error);
-      };
-      const chunks: Buffer[] = [];
-
-      stream.on('data', (chunk: Buffer) => {
-        chunks.push(chunk);
-      });
-
-      stream.once('end', async () => {
-        const fileBuffer = Buffer.concat(chunks);
-
-        try {
-          const uploaded = await this.putBlob(key, fileBuffer);
-
-          resolve(uploaded);
-        } catch (error) {
-          handleError(new InternalServerErrorException(error));
-        }
-      });
-
-      stream.on('error', (error) => handleError(new InternalServerErrorException(error)));
-    });
-    return file;
   }
 }
